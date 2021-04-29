2a121680fb272a3593a00d6f9017452f76f0f1ba
Harshit Verma <hverma1@cs.iitr.ac.in>
2021-04-29 20:06:53 +0530

    feat(migrator): made migrator_run_interval value dynamic

--- migrator/migrator.go
+++ migrator/migrator.go
@@ -1,32 +1,47 @@
 package migrator
 
 import (
-	"time"
 	"fmt"
+	"math"
 	"sync"
+	"time"
 )
 
 var Temp_to_storage_policy map[string]string = map[string]string{
-	"SUMMER" : "ALL_SSD",
-	"FALL" : "ONE_SSD",
-	"WINTER" : "WARM",
-	"FROZEN" : "COLD",
-	"N/A" : "HOT",
+	"SUMMER": "ALL_SSD",
+	"FALL":   "ONE_SSD",
+	"WINTER": "WARM",
+	"FROZEN": "COLD",
+	"N/A":    "HOT",
 }
 
-var migrator_run_interval time.Duration = time.Minute
+var migrator_run_interval time.Duration = time.Hour
 
 func StartMigrator(storagePolicy map[string]string, fileAccess map[string][]time.Time, fileAge map[string]time.Time, mutex *sync.Mutex) {
-	for _= range time.Tick(migrator_run_interval){
+	totalAccessInADay := 0
+
+	for _ = range time.Tick(migrator_run_interval) {
+		// lock the mutex
 		mutex.Lock()
 		for filename, accessTimes := range fileAccess {
+			// get count metrics
 			count_d, count_w, count_m := getCountMetrics(accessTimes)
-			fmt.Println(filename)
-			fmt.Println(count_d)
+			// add count_d value to get avg in the end
+			totalAccessInADay += count_d
+			// get new temperature of the file
 			temperature := getTemperature(count_d, count_w, count_m, fileAge[filename])
+			// update the storage policy depending on the temperature
 			storagePolicy[filename] = Temp_to_storage_policy[temperature]
 		}
+		// unlock the mutex
 		mutex.Unlock()
-		fmt.Println(storagePolicy)
+		// update the migrator run interval time by 24hrs/totalAccessInADay
+		timeTakenForOneAccess := fmt.Sprintf("%f", math.Min(float64((24*3600)/totalAccessInADay), 60)) + "s"
+		// update the migrator run interval value(it should not go beyond one minute)
+		if (24*3600)/totalAccessInADay > 60 {
+			migrator_run_interval, _ = time.ParseDuration(timeTakenForOneAccess)
+		} else {
+			migrator_run_interval = time.Minute
+		}
 	}
 }
