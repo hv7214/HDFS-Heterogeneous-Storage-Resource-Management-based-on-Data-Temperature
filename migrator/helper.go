085eec43f6689bed723cd1af7c05f03ec74f26a1
Harshit Verma <hverma1@cs.iitr.ac.in>
2021-04-29 10:46:04 +0530

    feat(migrator): add helper and migrator(partial) code

---
+++ migrator/helper.go
@@ -0,0 +1,46 @@
+package migrator
+
+import "time"
+
+var dayMilliseconds int64 = int64(86400000)
+var weekMilliseconds int64 = int64(604800000)
+var monthMilliseconds int64 = int64(2592000000)
+var yearMilliseconds int64 = int64(31536000000)
+
+func getCountMetrics(accessTimes []time.Time) (int, int, int) {
+	count_m := len(accessTimes)
+	count_w := 0
+	count_d := 0
+	boolWeek := false
+	boolDay := false
+
+	for ind := 0; ind < len(accessTimes); ind++ {
+		if time.Since(accessTimes[ind]).Milliseconds() <= weekMilliseconds && !boolWeek {
+			count_w = len(accessTimes) - ind + 1
+			boolWeek = true
+		}
+		if time.Since(accessTimes[ind]).Milliseconds() <= dayMilliseconds && !boolDay {
+			count_d = len(accessTimes) - ind + 1
+			boolDay = true
+		}
+	}
+
+	return count_d, count_w, count_m
+}
+
+func getTemperature(count_d int, count_m int, count_w int, age time.Time) string {
+	temperature := "N/A"
+	ageInMilliseconds := time.Since(age).Milliseconds()
+
+	if ageInMilliseconds < weekMilliseconds && count_d > 30 {
+		temperature = "SUMMER"
+	} else if ageInMilliseconds > weekMilliseconds && ageInMilliseconds < monthMilliseconds && count_d > 15 && count_w > 30 {
+		temperature = "FALL"
+	} else if ageInMilliseconds > monthMilliseconds && ageInMilliseconds < 3*monthMilliseconds && count_w == 0 && count_m > 0 {
+		temperature = "WINTER"
+	} else if ageInMilliseconds > 3*monthMilliseconds && ageInMilliseconds < yearMilliseconds && count_m == 0 {
+		temperature = "FROZEN"
+	}
+
+	return temperature
+}
--- migrator/migrator.go
+++ migrator/migrator.go
@@ -1,1 +1,10 @@
 package migrator
+
+import "time"
+
+func startMigrator(storagePolicy map[string]string, fileAccess map[string][]time.Time, fileAge map[string]time.Time) {
+	for filename, accessTimes := range fileAccess {
+		count_d, count_w, count_m := getCountMetrics(accessTimes)
+		temperature := getTemperature(count_d, count_w, count_m, fileAge[filename])
+	}
+}
