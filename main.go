31386999b2a22d14d69ec55ec05cfc2c6f99a095
Harshit Verma <hverma1@cs.iitr.ac.in>
2021-04-29 21:38:48 +0530

    refactor(main.go): add comments

--- main.go
+++ main.go
@@ -1,19 +1,23 @@
 package main
 
 import (
-	"Heterogenous_SRM/watcher"
-	"time"
 	"Heterogenous_SRM/migrator"
+	"Heterogenous_SRM/watcher"
 	"sync"
+	"time"
 )
 
 func main() {
+	// declare the needed structures
 	done := make(chan bool)
 	fileAccessMap := make(map[string][]time.Time)
 	fileAge := make(map[string]time.Time)
 	storagePolicy := make(map[string]string)
 	var mutex = &sync.Mutex{}
+
+	// start migrator and watcher
 	go migrator.StartMigrator(storagePolicy, fileAccessMap, fileAge, mutex)
 	go watcher.WatcherFunc("./test", fileAccessMap, fileAge, mutex)
+
 	<-done
 }
