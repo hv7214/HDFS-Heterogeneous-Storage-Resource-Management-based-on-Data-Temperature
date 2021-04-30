ce058121fa7a42d03e21c62d19d04351ebcc653d
karan0299 <karansingh0299@gmail.com>
2021-04-30 22:58:39 +0530

    resolved a minor bug

--- watcher/fileWatcher.go
+++ watcher/fileWatcher.go
@@ -23,6 +23,8 @@ func WatcherFunc(path string, fileAccess map[string][]time.Time, fileAge map[str
 
 	go func() {
 		for {
+			pass := 0
+			prevfile := ""
 			select {
 			// watch for events
 			case event := <-watcher.Events:
@@ -32,6 +34,16 @@ func WatcherFunc(path string, fileAccess map[string][]time.Time, fileAge map[str
 
 				if fileName == dir {
 					continue
+				}
+
+				if pass > 0 && fileName == prevfile {
+					pass = pass - 1
+					continue
+				}
+
+				if eventName == "WRITE" {
+					prevfile = fileName
+					pass = 3
 				}
 
 				capTimeStampsForOneMonth(fileAccess, fileName, mutex)
